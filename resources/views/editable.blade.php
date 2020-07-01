ddf1aabc38b48238d1b9bb1381f62b52c4b562a2
Mark Salmon <marksalmon@mark.salmon@mediconesystems.com>
2020-07-01 21:44:19 +0100

    wip

--- resources/views/editable.blade.php
+++ resources/views/editable.blade.php
@@ -11,10 +11,10 @@
             }
         })
     }
-}" x-init="init()" >
+}" x-init="init()" :key="{{ $rowId }}">
     <div x-on:edited.window></div>
-    <span x-bind:class="{ 'text-green-600': edited }" x-show="!edit" x-on:click="edit = true">{{ $value }}</span>
+    <span x-bind:class="{ 'text-green-600': edited }" x-show="!edit" x-on:click="edit = true; $refs.input.focus()">{{ $value }}</span>
     <span x-show="edit">
-        <input class="" value="{{ $value }}" wire:change="edited($event.target.value, '{{ $table }}', '{{ $column }}', '{{ $rowId }}')" x-on:click.away="edit = false" x-on:blur="edit = false" x-on:keydown.enter="edit = false" />
+        <input class="bg-transparent" x-ref="input" value="{{ $value }}" wire:change="edited($event.target.value, '{{ $table }}', '{{ $column }}', '{{ $rowId }}')" x-on:click.away="edit = false" x-on:blur="edit = false" x-on:keydown.enter="edit = false" />
     </span>
 </div>
