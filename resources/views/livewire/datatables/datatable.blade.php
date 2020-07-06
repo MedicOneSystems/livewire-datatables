65dd42027198c0cde430260b4e4afe47251dcf7b
Mark Salmon <mark.salmon@mediconesystems.com>
2020-07-06 22:50:16 +0100

    Added artisan command (#6)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * time filters
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * sorted pubishing
    
    * readme
    
    * tests passing
    
    * wip
    
    * added commands
    
    * readme
    
    Co-authored-by: Mark Salmon <marksalmon@mark.salmon@mediconesystems.com>

--- resources/views/livewire/datatables/datatable.blade.php
+++ resources/views/livewire/datatables/datatable.blade.php
@@ -25,11 +25,10 @@
             <div class="table align-middle min-w-full">
                 @unless($this->hideHeader)
                 <div class="table-row divide-x divide-gray-200">
-                    {{-- @foreach($this->visibleColumns as $index => $column) --}}
-                    @foreach($columns as $index => $column)
-                    @if($column['hidden'])
+                    @foreach($this->columns as $index => $column)
+
                     <div wire:click.prefetch="toggle('{{ $index }}')"
-                        class="relative table-cell h-12 w-3 bg-blue-100 hover:bg-blue-300 overflow-none align-top group"
+                        class="@if($column['hidden']) relative table-cell h-12 w-3 bg-blue-100 hover:bg-blue-300 overflow-none align-top group @else hidden @endif"
                         style="min-width:12px max:width:12px">
                         <button class="relative h-12 w-3 focus:outline-none">
                             <span
@@ -42,8 +41,8 @@
                             <path stroke-miterlimit="10" d="M313.66 206.75H.5V1.49l157.65 204.9L313.66 1.49v205.26z" />
                         </svg>
                     </div>
-                    @else
-                    <div class="relative table-cell h-12 overflow-hidden align-top">
+                    <div
+                        class="@if($column['hidden']) hidden @else relative table-cell h-12 overflow-hidden align-top @endif">
                         <button wire:click.prefetch="sort('{{ $index }}')"
                             class="w-full h-full px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider flex justify-between items-center focus:outline-none">
                             <span class="inline ">{{ str_replace('_', ' ', $column['label']) }}</span>
@@ -62,7 +61,6 @@
                             <x-icons.arrow-circle-left class="h-3 w-3 text-gray-300 hover:text-blue-400" />
                         </button>
                     </div>
-                    @endif
                     @endforeach
                 </div>
 
@@ -143,222 +141,16 @@
         @endif
     </div>
 
-    {{--
-    @if($this->activeFilters)
-    <div class="mt-4 p-4 rounded overflow-hidden align-middle min-w-full shadow sm:rounded-lg border-b border-gray-200 bg-white">
-        <div class="h-6 flex justify-between items-center">
-            <label class="uppercase tracking-wide text-blue-600 text-lg">Active Filters</label>
-            <button wire:click="clearAllFilters" class="px-2 py-1 flex items-center uppercase tracking-wide border-2 border-transparent hover:bg-red-400 text-gray-600 hover:text-white rounded-full focus:outline-none text-xs space-x-1">
-                <span>CLEAR ALL</span>
-                <x-icons.x-circle />
-            </button>
-        </div>
-        <div class="flex flex-wrap mt-2 -mx-2">
-            @if(isset($dates['column']) && ((isset($dates['start']) || isset($dates['end']))))
-            <button wire:click="clearDateFilter" class="px-2 py-1 flex items-center uppercase tracking-wide border-2 border-transparent hover:bg-red-400 text-gray-600 hover:text-white rounded-full focus:outline-none text-xs space-x-1">
-                <span class="">{{ $this->getColumnColumn($dates['column']) . ' between ' . Carbon\Carbon::parse($dates['start'] ?? '2000-01-01')->format('d/m/Y') . ' and ' . Carbon\Carbon::parse($dates['end'] ?? now())->format('d/m/Y') }}</span>
-    <x-icons.x-circle />
-    </button>
-    @endif
 
-    @if(isset($times['column']) && ((isset($times['start']) || isset($times['end']))))
-    <button wire:click="clearTimeFilter"
-        class="px-2 py-1 flex items-center uppercase tracking-wide border-2 border-transparent hover:bg-red-400 text-gray-600 hover:text-white rounded-full focus:outline-none text-xs space-x-1">
-        <span
-            class="">{{ $this->getColumnColumn($times['column'])  . ' between ' . ($times['start'] ?? '00:00') . ' and ' . ($times['end'] ?? '23:59') }}</span>
-        <x-icons.x-circle />
-    </button>
-    @endif
-    @foreach($activeSelectFilters as $index => $activeSelectFilter)
-    @foreach($activeSelectFilter as $key => $value)
-    <button wire:click="removeSelectFilter('{{ $index }}', '{{ $key }}')"
-        class="px-2 py-1 flex items-center uppercase tracking-wide border-2 border-transparent hover:bg-red-400 text-gray-600 hover:text-white rounded-full focus:outline-none text-xs space-x-1">
-        <span>{{ $this->getColumnLabel($index) . ": " . $this->getDisplayValue($index, $value) }}</span>
-        <x-icons.x-circle />
-    </button>
-    @endforeach
-    @endforeach
-    @foreach($activeBooleanFilters as $index => $activeBooleanFilter)
-    <button wire:click=" removeBooleanFilter('{{ $index }}')"
-        class="px-2 py-1 flex items-center uppercase tracking-wide border-2 border-transparent hover:bg-red-400 text-gray-600 hover:text-white rounded-full focus:outline-none text-xs space-x-1">
-        <span class="">{{ $this->getColumnLabel($index) . ": " . ($activeBooleanFilter == 1 ? 'Yes' : 'No') }}</span>
-        <x-icons.x-circle />
-    </button>
-    @endforeach
-    @foreach($activeTextFilters as $index => $activeTextFilter)
-    <button wire:click="removeTextFilter('{{ $index }}')"
-        class="px-2 py-1 flex items-center uppercase tracking-wide border-2 border-transparent hover:bg-red-400 text-gray-600 hover:text-white rounded-full focus:outline-none text-xs space-x-1">
-        <span class="">{{ $this->getColumnLabel($index) . ": " . $activeTextFilter }}</span>
-        <x-icons.x-circle />
-    </button>
-    @endforeach
-    @foreach($activeNumberFilters as $index => $activeNumberFilter)
-    <button wire:click="removeNumberFilter('{{ $index }}')"
-        class="px-2 py-1 flex items-center uppercase tracking-wide border-2 border-transparent hover:bg-red-400 text-gray-600 hover:text-white rounded-full focus:outline-none text-xs space-x-1">
-        <span
-            class="">{{ $this->getColumnLabel($index) . ": between " . ($activeNumberFilter['start'] ?? '0') . " and " . ($activeNumberFilter['end'] ?? 'max') }}</span>
-        <x-icons.x-circle />
-    </button>
-    @endforeach
-</div>
-</div>
-@endif
-
-@if(count($this->dateFilters))
-<div
-    class="mt-4 p-4 rounded overflow-hidden align-middle min-w-full shadow sm:rounded-lg border-b border-gray-200 bg-white">
-    <div class="h-6 flex justify-between items-center">
-        <label class="uppercase tracking-wide text-blue-600 text-lg">Date Range</label>
-        class="@if(!isset($dates['column']) || $dates['column'] === '') hidden @endif px-2 py-1 flex items-center
-        uppercase tracking-wide border-2 border-transparent hover:bg-red-400 text-gray-600 hover:text-white rounded-full
-        focus:outline-none text-xs space-x-1">
-        <span>CLEAR</span>
-        <x-icons.x-circle />
-        </button>
-    </div>
-    <div class="xl:grid grid-cols-2 gap-4">
-        <div class="mt-2 grid grid-cols-3 gap-4">
-            <select name="dateColumn" wire:model="dates.column" class="w-full form-select">
-                <option></option>
-                @foreach($this->dateFilters as $index => $column)
-                <option value="{{ $index }}">{{ $column['label'] }}</option>
-                @endforeach
-            </select>
-
-            <input type="date" name="start" wire:model="dates.start" class="w-full form-input" />
-            <input type="date" name='end' wire:model="dates.end" class="w-full form-input" />
-        </div>
-        <div class="mt-4 xl:mt-2 grid grid-cols-3 md:grid-cols-6 gap-2">
-            @foreach(get_class_methods(Mediconesystems\LivewireDatatables\Traits\WithPresetDateFilters::class) as
-            $preset)
-            <button
-                class="px-3 py-2 rounded text-white text-xs uppercase tracking-wide focus:outline-none bg-blue-500 hover:bg-blue-800"
-                wire:click="{{ $preset }}">
-                {{ implode(' ', preg_split('/(?=[A-Z])/', $preset)) }}
-            </button>
-            @endforeach
-        </div>
-    </div>
-</div>
-@endif
-
-@if(count($this->timeFilters))
-<div
-    class="mt-4 p-4 rounded overflow-hidden align-middle min-w-full shadow sm:rounded-lg border-b border-gray-200 bg-white">
-    <div class="h-6 flex justify-between items-center">
-        <label class="uppercase tracking-wide text-blue-600 text-lg">Time Range</label>
-        <button wire:click="clearTimeFilter"
-            class="@if(!isset($times['column']) || $times['column'] === '') hidden @endif px-2 py-1 flex items-center uppercase tracking-wide border-2 border-transparent hover:bg-red-400 text-gray-600 hover:text-white rounded-full focus:outline-none text-xs space-x-1">
-            <span>CLEAR</span>
-            <x-icons.x-circle />
-        </button>
-    </div>
-    <div class="xl:grid grid-cols-2 gap-4">
-        <div class="mt-2 grid grid-cols-3 gap-4">
-            <select name="timeColumn" wire:model="times.column" class="w-full form-select">
-                <option></option>
-                @foreach($this->timeFilters as $index => $column)
-                <option value="{{ $index }}">{{ $column['label']}}</option>
-                @endforeach
-            </select>
-            <input type="time" name="start" wire:model="times.start" class="w-full form-input">
-            <input type="time" name="end" wire:model="times.end" class="w-full form-input">
-        </div>
-        <div class="mt-4 xl:mt-2 grid grid-cols-3 md:grid-cols-6 gap-2">
-            @foreach(get_class_methods(Mediconesystems\LivewireDatatables\Traits\WithPresetTimeFilters::class) as
-            $preset)
-            <button
-                class="px-3 py-2 rounded text-white text-xs uppercase tracking-wide focus:outline-none bg-blue-500 hover:bg-blue-800"
-                wire:click="{{ $preset }}">
-                {{ implode(' ', preg_split('/(?=[A-Z])/', $preset)) }}
-            </button>
-            @endforeach
-        </div>
-    </div>
-</div>
-@endif
-
-@if(count($this->selectFilters) || count($this->booleanFilters) || count($this->textFilters) ||
-count($this->numberFilters))
-<div
-    class="mt-4 p-4 rounded overflow-hidden align-middle min-w-full shadow sm:rounded-lg border-b border-gray-200 bg-white">
-    <div class="h-6 flex justify-between items-center">
-        <label class="uppercase tracking-wide text-blue-600 text-lg">Filters</label>
-        <button wire:click="clearFilter"
-            class="@unless(count($this->activeSelectFilters) || count($this->activeBooleanFilters) || count($this->activeTextFilters)) hidden @endif px-2 py-1 flex items-center uppercase tracking-wide border-2 border-transparent hover:bg-red-400 text-gray-600 hover:text-white rounded-full focus:outline-none text-xs space-x-1">
-            <span>CLEAR</span>
-            <x-icons.x-circle />
-        </button>
-    </div>
-
-    <div class="mt-2 grid grid-cols-3 gap-4">
-        @foreach($this->selectFilters as $i => $filter)
-        <div class="w-full relative">
-            <label class="uppercase tracking-wide text-gray-600 text-xs py-1 rounded flex justify-between"
-                for="{{ $filter['label'] }}">
-                {{ ucwords(str_replace('_', ' ', str_replace('_id', '', $filter['label']))) }}
-            </label>
-            <div class="">
-
+    <div wire:loading>
+        <div class="fixed z-50 bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center">
+            <div x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
+                x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200"
+                x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+                class="fixed inset-0 transition-opacity">
+                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
+            <x-icons.cog class="h-36 w-36 spinner" />
         </div>
-        @endforeach
-
-
-        @foreach($this->booleanFilters as $i => $filter)
-        <div class="w-full relative">
-            <label class="uppercase tracking-wide text-gray-600 text-xs py-1 rounded flex justify-between"
-                for="{{ $filter['label'] }}">
-                {{ ucwords(str_replace('_', ' ', str_replace('_id', '', $filter['label']))) }}
-            </label>
-            <div class="relative">
-                <select name="{{ $filter['label'] }}" class="w-full form-select"
-                    wire:input="doBooleanFilter('{{ $i }}', $event.target.value)">
-                    <option value=""></option>
-                    <option value="0">No</option>
-                    <option value="1">Yes</option>
-                </select>
-            </div>
-        </div>
-        @endforeach
-
-        @foreach($this->textFilters as $i => $filter)
-        <div class="w-full relative">
-            <label class="uppercase tracking-wide text-gray-600 text-xs py-1 rounded flex justify-between"
-                for="{{ $filter['label'] }}">
-                <span>{{ ucwords(str_replace('_', ' ', $filter['label'])) }}</span>
-            </label>
-            <div class="relative">
-                <input name="{{ $filter['label'] }}" type="text" class="w-full form-input"
-                    wire:input.lazy="doTextFilter('{{ $i }}', $event.target.value)" />
-            </div>
-        </div>
-        @endforeach
-
-        @foreach($this->numberFilters as $i => $filter)
-        <div class="w-full relative">
-            <label class="uppercase tracking-wide text-gray-600 text-xs py-1 rounded flex justify-between"
-                for="{{ $filter['label'] }}">
-                <span>{{ ucwords(str_replace('_', ' ', $filter['label'])) }}</span>
-            </label>
-
-        </div>
-        @endforeach
-
     </div>
-</div>
-@endif
---}}
-
-<div wire:loading>
-    <div class="fixed z-50 bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center">
-        <div x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
-            x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200"
-            x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-            class="fixed inset-0 transition-opacity">
-            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-        <x-icons.cog class="h-36 w-36 spinner" />
-    </div>
-</div>
 </div>
