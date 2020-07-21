d0cd66dee85b20f37a4c40d4e2ffe0a2e80d79c2
Mark Salmon <marksalmon@mark.salmon@mediconesystems.com>
2020-07-21 15:44:23 +0100

    clear select

--- resources/views/livewire/datatables/filters/select.blade.php
+++ resources/views/livewire/datatables/filters/select.blade.php
@@ -19,7 +19,7 @@
 
     <div class="flex flex-wrap max-w-48 space-x-1">
         @foreach($this->activeSelectFilters[$index] ?? [] as $key => $value)
-        <button wire:click="removeSelectFilter('{{ $index }}', '{{ $key }}')"
+        <button wire:click="removeSelectFilter('{{ $index }}', '{{ $key }}')" x-on:click="$refs.select.value=''"
             class="m-1 pl-1 flex items-center uppercase tracking-wide bg-gray-300 text-white hover:bg-red-600 rounded-full focus:outline-none text-xs space-x-1">
             <span>{{ $this->getDisplayValue($index, $value) }}</span>
             <x-icons.x-circle />
