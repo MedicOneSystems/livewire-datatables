8bf0c99e2a1256fcbe1df7927dcef251fc08b57d
Mark Salmon <mark.salmon@mediconesystems.com>
2020-09-24 16:01:53 +0100

    Apply fixes from StyleCI (#46)
    Co-authored-by: Mark Salmon <marksalmon@users.noreply.github.com>

--- src/Http/Livewire/LivewireDatatable.php
+++ src/Http/Livewire/LivewireDatatable.php
@@ -700,7 +700,7 @@ class LivewireDatatable extends Component
 
     public function addGlobalSearch()
     {
-        if (!$this->search) {
+        if (! $this->search) {
             return $this;
         }
 
@@ -712,10 +712,10 @@ class LivewireDatatable extends Component
                             foreach ($this->getColumnField($i) as $column) {
                                 $query->when(is_array($column), function ($query) use ($search, $column) {
                                     foreach ($column as $col) {
-                                        $query->orWhereRaw('LOWER(' . $col . ') like ?', "%$search%");
+                                        $query->orWhereRaw('LOWER('.$col.') like ?', "%$search%");
                                     }
                                 }, function ($query) use ($search, $column) {
-                                    $query->orWhereRaw('LOWER(' . $column . ') like ?', "%$search%");
+                                    $query->orWhereRaw('LOWER('.$column.') like ?', "%$search%");
                                 });
                             }
                         });
