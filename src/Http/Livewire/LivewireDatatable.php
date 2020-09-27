a3a3741b63b2cf8b82ab46b02a2651393749cf43
Oussama <imo.ossama1@gmail.com>
2020-09-28 00:46:21 +0100

    Eloquent Refactoring

--- src/Http/Livewire/LivewireDatatable.php
+++ src/Http/Livewire/LivewireDatatable.php
@@ -688,7 +688,7 @@ class LivewireDatatable extends Component
     
     public function addGlobalSearch()
     {
-        if (!$this->search) {
+        if (! $this->search) {
             return $this;
         }
 
@@ -700,10 +700,10 @@ class LivewireDatatable extends Component
                             foreach ($this->getColumnField($i) as $column) {
                                 $query->when(is_array($column), function ($query) use ($search, $column) {
                                     foreach ($column as $col) {
-                                        $query->orWhereRaw('LOWER(' . $col . ') like ?', "%$search%");
+                                        $query->orWhereRaw('LOWER('.$col.') like ?', "%$search%");
                                     }
                                 }, function ($query) use ($search, $column) {
-                                    $query->orWhereRaw('LOWER(' . $column . ') like ?', "%$search%");
+                                    $query->orWhereRaw('LOWER('.$column.') like ?', "%$search%");
                                 });
                             }
                         });
