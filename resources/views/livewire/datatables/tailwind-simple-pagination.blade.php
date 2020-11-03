b292acfb82a68b900f2cbf138d79a0f571b46db6
Mark Salmon <marksalmon@marks-mbp.lan>
2020-11-03 15:38:58 +0000

    fix mobile paginator links

--- resources/views/livewire/datatables/tailwind-simple-pagination.blade.php
+++ resources/views/livewire/datatables/tailwind-simple-pagination.blade.php
@@ -6,19 +6,19 @@
     Previous
 </div>
 @else
-<a class="w-32 flex justify-between items-center relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" href="{{ $paginator->previousPageUrl() }}" rel="prev">
+<button wire:click="previousPage" class="w-32 flex justify-between items-center relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
     <x-icons.arrow-left />
     Previous
-</a>
+</button>
 @endif
 
 
 <!-- Next Page pnk -->
 @if ($paginator->hasMorePages())
-<a class="w-32 flex justify-between items-center relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" href="{{ $paginator->nextPageUrl() }}" rel="next">
+<button wire:click="nextPage" class="w-32 flex justify-between items-center relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
     Next
     <x-icons.arrow-right />
-</a>
+</button>
 @else
 <div class="w-32 flex justify-between items-center relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-400 bg-gray-50">
     Next
