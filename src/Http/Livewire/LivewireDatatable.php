d7ac4b8a937773e42548bea173d92928c44b919e
jjrohrer <jj.bitbucket@rohrer.org>
2020-11-05 18:30:48 -0500

    Make possible to disable highlighting

--- src/Http/Livewire/LivewireDatatable.php
+++ src/Http/Livewire/LivewireDatatable.php
@@ -931,7 +931,7 @@ class LivewireDatatable extends Component
                     $row->$name = $this->callbacks[$name]($value, $row);
                 }
 
-                if ($this->search && $this->searchableColumns()->firstWhere('name', $name)) {
+                if ($this->search && ! config('livewire-datatables.suppress_search_highlights') && $this->searchableColumns()->firstWhere('name', $name)) {
                     $row->$name = $this->highlight($row->$name, $this->search);
                 }
             }
@@ -947,6 +947,34 @@ class LivewireDatatable extends Component
         return is_array($this->freshColumns[$index]['filterable']) && is_numeric($value)
             ? collect($this->freshColumns[$index]['filterable'])->firstWhere('id', '=', $value)['name'] ?? $value
             : $value;
+    }
+
+
+    /*  This can be called to apply highlting of the search term to some string.
+     *  Motivation: Call this from your Column::Callback to apply highlight to a chosen section of the result.
+     */
+    public function highlightStringWithCurrentSearchTerm(string $originalString)
+    {
+        if (!$this->search) {
+            return $originalString;
+        } else {
+            return static::highlightString($originalString, $this->search);
+        }
+    }
+
+    /* Utility function for applying highlighting to given string */
+    public static function highlightString(string $originalString, string $searchingForThisSubstring)
+    {
+        $searchStringNicelyHighlightedWithHtml = view(
+            'datatables::highlight',
+            ['slot' => $searchingForThisSubstring]
+        )->render();
+        $stringWithHighlightedSubstring = str_ireplace(
+            $searchingForThisSubstring,
+            $searchStringNicelyHighlightedWithHtml,
+            $originalString
+        );
+        return $stringWithHighlightedSubstring;
     }
 
     public function highlight($value, $string)
