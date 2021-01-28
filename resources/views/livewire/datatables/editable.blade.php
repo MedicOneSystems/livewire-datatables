9388aec62ada8b59987d9a15f1fe54d3676de2ae
waffshappen <44290023+waffshappen@users.noreply.github.com>
2021-01-28 19:18:19 +0000

    Values read from the DB should be escaped default (#69)
    Raw html output that can cause simple takeover by displaying user-entered data should be the hard path (another modifier), not the easy one.
    Example:
    Display results from a table containing <script>alert('woof!');</script>

--- resources/views/livewire/datatables/editable.blade.php
+++ resources/views/livewire/datatables/editable.blade.php
@@ -13,9 +13,9 @@
     }
 }" x-init="init()" :key="{{ $rowId }}">
     <button class="w-full text-left hover:bg-blue-100 px-2 py-1 -mx-2 -my-1 rounded focus:outline-none" x-bind:class="{ 'text-green-600': edited }" x-show="!edit"
-        x-on:click="edit = true; $nextTick(() => { $refs.input.focus() })">{!! $value !!}</button>
+        x-on:click="edit = true; $nextTick(() => { $refs.input.focus() })">{!! htmlspecialchars($value) !!}</button>
     <span x-cloak x-show="edit">
-        <input class="border-blue-400 px-2 py-1 -mx-2 -my-1 rounded focus:outline-none focus:border" x-ref="input" value="{!! strip_tags($value) !!}"
+        <input class="border-blue-400 px-2 py-1 -mx-2 -my-1 rounded focus:outline-none focus:border" x-ref="input" value="{!! htmlspecialchars($value) !!}"
             wire:change="edited($event.target.value, '{{ $table }}', '{{ $column }}', '{{ $rowId }}')"
             x-on:click.away="edit = false" x-on:blur="edit = false" x-on:keydown.enter="edit = false" />
     </span>
