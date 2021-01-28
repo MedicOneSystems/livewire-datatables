2db0794934c49ff9f66dc7701f3bc4e852388294
Mark Salmon <mark.salmon@mediconesystems.com>
2021-01-28 13:59:15 +0000

    wip

--- resources/views/livewire/datatables/filters/select.blade.php
+++ resources/views/livewire/datatables/filters/select.blade.php
@@ -3,7 +3,7 @@
         <select
             x-ref="select"
             name="{{ $name }}"
-            class="m-1 text-sm leading-4 flex-grow form-select"
+            class="text-sm leading-4 block rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
             wire:input="doSelectFilter('{{ $index }}', $event.target.value)"
             x-on:input="$refs.select.value=''"
         >
