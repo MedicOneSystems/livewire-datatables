0ccea80623a2ba46aaa979954144af0afef14ece
manojbhatta <jmsbhatta@gmail.com>
2021-02-02 23:04:08 +0545

    remove duplicate classes

--- resources/views/livewire/datatables/tooltip.blade.php
+++ resources/views/livewire/datatables/tooltip.blade.php
@@ -1,4 +1,4 @@
 <span class="relative group cursor-pointer">
-    <span class="inline-block flex items-center">{{ Str::limit($slot, $length) }}</span>
-    <span class="hidden group-hover:block absolute z-10 -ml-28 w-96 mt-2 p-2 text-xs whitespace-pre-wrap rounded-lg bg-gray-100 border border-gray-300 shadow-xl text-gray-700 text-left whitespace-normal">{{ $slot }}</span>
+    <span class="flex items-center">{{ Str::limit($slot, $length) }}</span>
+    <span class="hidden group-hover:block absolute z-10 -ml-28 w-96 mt-2 p-2 text-xs whitespace-pre-wrap rounded-lg bg-gray-100 border border-gray-300 shadow-xl text-gray-700 text-left">{{ $slot }}</span>
 </span>
