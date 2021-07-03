4c3f40b7e64c1caed0dd7818054a907d411c3a12
Kevin McKee <mckee.kevin@gmail.com>
2021-07-03 04:52:10 -0500

    Add model_namespace to config (#140)
    I went source diving and found this option can be used in the config, so I think it would be easier for users to see the default in the config so they know they can easily change it.

--- config/livewire-datatables.php
+++ config/livewire-datatables.php
@@ -4,4 +4,5 @@ return [
     'default_time_format' => 'H:i',
     'default_date_format' => 'd/m/Y',
     'suppress_search_highlights' => false, // When searching, don't highlight matching search results when set to true
+    'model_namespace' => 'App',
 ];
