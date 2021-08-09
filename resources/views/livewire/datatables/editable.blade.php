0c84b9b91ffec3b9fe8f5d5b9048b97920c2e9b6
Mark Salmon <mark.salmon@mediconesystems.com>
2021-08-09 22:18:08 +0100

    min height on editable input #245

--- resources/views/livewire/datatables/editable.blade.php
+++ resources/views/livewire/datatables/editable.blade.php
@@ -13,7 +13,7 @@
     }
 }" x-init="init()" :key="{{ $rowId }}">
     <button class="w-full text-left hover:bg-blue-100 px-2 py-1 -mx-2 -my-1 rounded focus:outline-none" x-bind:class="{ 'text-green-600': edited }" x-show="!edit"
-        x-on:click="edit = true; $nextTick(() => { $refs.input.focus() })">{!! htmlspecialchars($value) !!}</button>
+        x-on:click="edit = true; $nextTick(() => { $refs.input.focus() })" style="min-height:30px">{!! htmlspecialchars($value) !!}</button>
     <span x-cloak x-show="edit">
         <input class="border-blue-400 px-2 py-1 -mx-2 -my-1 rounded focus:outline-none focus:border" x-ref="input" value="{!! htmlspecialchars($value) !!}"
             wire:change="edited($event.target.value, '{{ $table }}', '{{ $column }}', '{{ $rowId }}')"
