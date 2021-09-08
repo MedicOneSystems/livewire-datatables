8886836e68b07e91fbd1aecb8a71e09ed6d21ed7
Mark Salmon <marksalmon@users.noreply.github.com>
2021-09-08 16:05:53 +0000

    Apply fixes from StyleCI

--- src/Http/Livewire/LivewireDatatable.php
+++ src/Http/Livewire/LivewireDatatable.php
@@ -1294,7 +1294,7 @@ class LivewireDatatable extends Component
     public function isRtl($value)
     {
         $rtlChar = '/[\x{0590}-\x{083F}]|[\x{08A0}-\x{08FF}]|[\x{FB1D}-\x{FDFF}]|[\x{FE70}-\x{FEFF}]/u';
-        
+
         return preg_match($rtlChar, $value) != 0;
     }
 
