72fe18a3bb49b8f025046295b7e28ad03655fc87
Siddhartha Mehta <siddharthamehta18@gmail.com>
2021-11-16 16:31:14 +0530

    Add format method to Number Column.
    formats & rounds a number with grouped thousands - 1000000 => 1,000,000.00 (If places set to 2) with variable places.

--- src/NumberColumn.php
+++ src/NumberColumn.php
@@ -18,4 +18,15 @@ class NumberColumn extends Column
 
         return $this;
     }
+    
+    // formats & rounds a number with grouped thousands - 1000000 => 1,000,000.00 (If places set to 2)
+    public function format($places = 2)
+    {
+
+        $this->callback = function ($value) use ($places) {
+            return number_format($value, $places, '.', ',');
+        };
+
+        return $this;
+    }
 }
