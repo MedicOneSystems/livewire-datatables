203315a611a3a29622f0d423f2ffdb3dfcfe1040
Herbert Maschke <thyseus@gmail.com>
2022-02-21 10:04:18 +0100

    add tooltip to views

--- resources/views/livewire/datatables/header-no-hide.blade.php
+++ resources/views/livewire/datatables/header-no-hide.blade.php
@@ -1,5 +1,7 @@
 @unless($column['hidden'])
-    <div class="relative table-cell h-12 overflow-hidden align-top" @include('datatables::style-width')>
+    <div
+        @if (isset($column['tooltip']['text'])) title="{{ $column['tooltip']['text'] }}" @endif
+        class="relative table-cell h-12 overflow-hidden align-top" @include('datatables::style-width')>
     @if($column['unsortable'])
         <div class="w-full h-full px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider flex items-center focus:outline-none @if($column['align'] === 'right') justify-end @elseif($column['align'] === 'center') justify-center @endif">
             <span class="inline ">{{ str_replace('_', ' ', $column['label']) }}</span>
@@ -10,9 +12,9 @@
             <span class="inline text-xs text-blue-400">
                 @if($sort === $index)
                     @if($direction)
-                        <x-icons.chevron-up wire:loading.remove class="h-6 w-6 text-green-600 stroke-current" />
+                        <x-icons.chevron-up wire:loading.remove class="w-6 h-6 text-green-600 stroke-current" />
                     @else
-                        <x-icons.chevron-down wire:loading.remove class="h-6 w-6 text-green-600 stroke-current" />
+                        <x-icons.chevron-down wire:loading.remove class="w-6 h-6 text-green-600 stroke-current" />
                     @endif
                 @endif
             </span>
