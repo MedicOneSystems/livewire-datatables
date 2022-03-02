6bdbb2229b50345b29962ce6f449bb134747058e
Romain GONÇALVES <romain.goncalves@gmail.com>
2022-03-02 20:16:52 +0100

    Takes into account hidden columns when displaying summary row

--- resources/views/livewire/datatables/datatable.blade.php
+++ resources/views/livewire/datatables/datatable.blade.php
@@ -186,13 +186,15 @@
                     @if ($this->hasSummaryRow())
                         <div class="table-row p-1">
                             @foreach($this->columns as $column)
-                                @if ($column['summary'])
-                                    <div class="table-cell px-6 py-2 whitespace-no-wrap @if($column['align'] === 'right') text-right @elseif($column['align'] === 'center') text-center @else text-left @endif {{ $this->cellClasses($row, $column) }}">
-                                        {{ $this->summarize($column['name']) }}
-                                    </div>
-                                @else
-                                    <div class="table-cell"></div>
-                                @endif
+                                @unless($column['hidden'])
+                                    @if ($column['summary'])
+                                        <div class="table-cell px-6 py-2 whitespace-no-wrap @if($column['align'] === 'right') text-right @elseif($column['align'] === 'center') text-center @else text-left @endif {{ $this->cellClasses($row, $column) }}">
+                                            {{ $this->summarize($column['name']) }}
+                                        </div>
+                                    @else
+                                        <div class="table-cell"></div>
+                                    @endif
+                                @endunless
                             @endforeach
                         </div>
                     @endif
