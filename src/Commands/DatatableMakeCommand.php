5c05fd602ccdd991738fdfd3f4cf25a1ee9a4995
Herbert Maschke <thyseus@gmail.com>
2022-03-22 15:44:12 +0100

    add alias to make:livewire-datatable command

---
+++ src/Commands/DatatableMakeCommand.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Mediconesystems\LivewireDatatables\Commands;
+
+class DatatableMakeCommand extends MakeDatatableCommand
+{
+    protected $signature = 'livewire:datatable {name} {--model=}';
+}
