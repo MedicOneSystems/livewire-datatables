b8bc51e67088961e46590ae9f28665651a1a5102
Renardo <rdd@renardudezert.com>
2022-03-31 10:46:49 +0200

    Bugfix toggleSelectAll() with groupBy() in query builder
    If we use groupBy() in the queryBuilder, the count() method in toggleSelectAll() return a wrong value. As a result, the toggleSelectAll() doesn't work as expected selecting all checkboxes works, but deselecting all of them does not).
    
    This is an already known issue : https://github.com/laravel/framework/issues/22883 and I use the approach https://github.com/laravel/framework/issues/22883#issuecomment-438521002 to fix the bug

--- src/Http/Livewire/LivewireDatatable.php
+++ src/Http/Livewire/LivewireDatatable.php
@@ -1696,7 +1696,7 @@ class LivewireDatatable extends Component
 
     public function toggleSelectAll()
     {
-        if (count($this->selected) === $this->getQuery()->count()) {
+        if (count($this->selected) === $this->getQuery()->getCountForPagination()) {
             $this->selected = [];
         } else {
             $this->selected = $this->checkboxQuery()->values()->toArray();
