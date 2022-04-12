6013fb13468aaedb94ae2f9502f4f69aed2e169a
Saulius Kazokas <saulens22@gmail.com>
2022-04-13 01:06:55 +0300

    Fix trait

--- src/Traits/WithCallbacks.php
+++ src/Traits/WithCallbacks.php
@@ -3,15 +3,18 @@
 namespace Mediconesystems\LivewireDatatables\Traits;
 
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 trait WithCallbacks
 {
     public function edited($value, $key, $column, $rowId)
     {
-        $this->connection()->table(Str::before($key, '.'))
+        DB::connection($this->connection())->table(Str::before($key, '.'))
             ->where(Str::after($key, '.'), $rowId)
             ->update([$column => $value]);
 
         $this->emit('fieldEdited', $rowId);
     }
+
+    public abstract function connection();
 }
