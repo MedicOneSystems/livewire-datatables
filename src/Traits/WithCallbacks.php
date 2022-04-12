7636e63faa9d7b410317133a7cdc7e8d797f1c30
Saulius Kazokas <saulens22@gmail.com>
2022-04-12 17:26:21 +0300

    Callback use defined connection

--- src/Traits/WithCallbacks.php
+++ src/Traits/WithCallbacks.php
@@ -2,14 +2,13 @@
 
 namespace Mediconesystems\LivewireDatatables\Traits;
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 trait WithCallbacks
 {
     public function edited($value, $key, $column, $rowId)
     {
-        DB::table(Str::before($key, '.'))
+        $this->connection()->table(Str::before($key, '.'))
             ->where(Str::after($key, '.'), $rowId)
             ->update([$column => $value]);
 
