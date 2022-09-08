28792d3239b83757ff2dd11647af79dee2a50267
kountouris7 <kountouris7@gmail.com>
2022-09-08 21:43:38 +0300

    Bug on header-no-hide.blade.php with $column['sortable']

--- resources/views/livewire/datatables/header-no-hide.blade.php
+++ resources/views/livewire/datatables/header-no-hide.blade.php
@@ -2,7 +2,7 @@
     <div
         @if (isset($column['tooltip']['text'])) title="{{ $column['tooltip']['text'] }}" @endif
                                                 class="relative table-cell h-12 overflow-hidden align-top" @include('datatables::style-width')>
-        @if($column['sortable'])
+        @if(!$column['sortable'])
             <div
                 class="w-full h-full px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider flex items-center focus:outline-none @if($column['headerAlign'] === 'right') justify-end @elseif($column['headerAlign'] === 'center') justify-center @endif">
                 <span class="inline ">{{ str_replace('_', ' ', $column['label']) }}</span>
