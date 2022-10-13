fba2f74e139ca2d58c5d0cb8ca48acd3002a9c47
Richard Dubiel <rickdubiel@gmail.com>
2022-10-13 16:56:27 -0500

    Adds hideable functionality to column select

--- resources/views/livewire/datatables/hide-column-multiselect.blade.php
+++ resources/views/livewire/datatables/hide-column-multiselect.blade.php
@@ -8,28 +8,30 @@
         <div x-show="show" x-on:click.away="show = false" class="z-50 absolute mt-16 -mr-4 shadow-2xl top-100 bg-white w-96 right-0 rounded max-h-select overflow-y-auto" x-cloak>
             <div class="flex flex-col w-full">
                 @foreach($this->columns as $index => $column)
-                <div>
-                    <div class="@unless($column['hidden']) hidden @endif cursor-pointer w-full border-gray-800 border-b bg-gray-700 text-gray-500 hover:bg-blue-600 hover:text-white" wire:click="toggle({{$index}})">
-                        <div class="relative flex w-full items-center p-2 group">
-                            <div class=" w-full items-center flex">
-                                <div class="mx-2 leading-6">{{ $column['label'] }}</div>
+                    @if ($column['hideable'] !== false)
+                        <div>
+                            <div class="@unless($column['hidden']) hidden @endif cursor-pointer w-full border-gray-800 border-b bg-gray-700 text-gray-500 hover:bg-blue-600 hover:text-white" wire:click="toggle({{$index}})">
+                                <div class="relative flex w-full items-center p-2 group">
+                                    <div class=" w-full items-center flex">
+                                        <div class="mx-2 leading-6">{{ $column['label'] }}</div>
+                                    </div>
+                                    <div class="absolute inset-y-0 right-0 pr-2 flex items-center">
+                                        <x-icons.check-circle class="h-3 w-3 stroke-current text-gray-700" />
+                                    </div>
+                                </div>
                             </div>
-                            <div class="absolute inset-y-0 right-0 pr-2 flex items-center">
-                                <x-icons.check-circle class="h-3 w-3 stroke-current text-gray-700" />
+                            <div class="@if($column['hidden']) hidden @endif cursor-pointer w-full border-gray-800 border-b bg-gray-700 text-white hover:bg-red-600" wire:click="toggle({{$index}})">
+                                <div class="relative flex w-full items-center p-2 group">
+                                    <div class=" w-full items-center flex">
+                                        <div class="mx-2 leading-6">{{ $column['label'] }}</div>
+                                    </div>
+                                    <div class="absolute inset-y-0 right-0 pr-2 flex items-center">
+                                        <x-icons.x-circle class="h-3 w-3 stroke-current text-gray-700" />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="@if($column['hidden']) hidden @endif cursor-pointer w-full border-gray-800 border-b bg-gray-700 text-white hover:bg-red-600" wire:click="toggle({{$index}})">
-                        <div class="relative flex w-full items-center p-2 group">
-                            <div class=" w-full items-center flex">
-                                <div class="mx-2 leading-6">{{ $column['label'] }}</div>
-                            </div>
-                            <div class="absolute inset-y-0 right-0 pr-2 flex items-center">
-                                <x-icons.x-circle class="h-3 w-3 stroke-current text-gray-700" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    @endif
                 @endforeach
             </div>
         </div>
