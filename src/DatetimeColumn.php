353d406c6f04d2d3141c3837a19854a81af6550b
Danilo Fernandes <danilofsouza@gmail.com>
2023-02-11 15:21:22 -0300

    Fix the issue detected by ci, and created a new config named "default_datetime_format"

--- src/DatetimeColumn.php
+++ src/DatetimeColumn.php
@@ -17,7 +17,7 @@ class DatetimeColumn extends Column
     public function format($format = null)
     {
         $this->callback = function ($value) use ($format) {
-            return $value ? Carbon::parse($value)->format($format ?? config('livewire-datatables.default_date_format'). " ". config('livewire-datatables.default_time_format')) : null;
+            return $value ? Carbon::parse($value)->format($format ?? config('livewire-datatables.default_datetime_format')) : null;
         };
 
         return $this;
